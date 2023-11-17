cb7acf66605d63ec3cd344fce6267bd5cb37659b
Alen Alduk <devalen@alens-mbp.speedport.ip>
2023-11-17 20:04:13 +0100

    changes on views

--- chat/views.py
+++ chat/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 def index(request):
     if request.method == 'POST':
         print('Recieved data' + request.POST['textmessage'])
-        return render(request, 'chat/index.html', {'username': 'Alen'})
+    return render(request, 'chat/index.html', {'username': 'Alen'})
