15b9dd0095e1734368a457a7f26641b6a906e083
Alen Alduk <devalen@alens-mbp.speedport.ip>
2023-11-18 12:19:49 +0100

    added chatbox

--- chat/admin.py
+++ chat/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import Message
+from .models import Chat, Message
 
 
 class MessageAdmin(admin.ModelAdmin):
-    fields = ('text', 'created_at', 'author', 'receiver')
+    fields = ('chat', 'text', 'created_at', 'author', 'receiver')
     list_display = ('created_at', 'author', 'text', 'receiver')
     search_fields = ('text',)
 
@@ -11,3 +11,4 @@ class MessageAdmin(admin.ModelAdmin):
 
 # Register your models here.
 admin.site.register(Message, MessageAdmin)
+admin.site.register(Chat)
--- chat/views.py
+++ chat/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from .models import Chat, Message
 
 def index(request):
     if request.method == 'POST':
-        print('Recieved data' + request.POST['textmessage'])
-    return render(request, 'chat/index.html', {'username': 'Alen'})
+        print('Recieved data ' + request.POST['textmessage'])
+        myChat = Chat.objects.get(id=1)
+        Message.objects.create(text = request.POST['textmessage'], chat = myChat, author = request.user, receiver = request.user)
+    chatMessages = Message.objects.filter(chat__id = 1)
+    return render(request, 'chat/index.html', {'messages': chatMessages})
