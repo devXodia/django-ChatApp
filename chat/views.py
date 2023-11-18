ae77c097e253f191d59a2a6394be397a678e4cf0
Alen Alduk <a.alduk@icloud.com>
2023-11-18 18:30:41 +0100

    added register form

--- chat/views.py
+++ chat/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 from .models import Chat, Message
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 
 @login_required(login_url='/login/')
 def index(request):
@@ -20,7 +22,22 @@ def login_view(request):
         user = authenticate(username = request.POST.get('username'), password = request.POST.get('password'))
         if user:
             login(request, user)
-            return HttpResponseRedirect(request.POST.get('redirect'))
+            return HttpResponseRedirect('/chat/')
         else:
             return render(request, 'auth/login.html', {'wrongPassword': True, 'redirect': redirect})
     return render(request, 'auth/login.html', {'redirect': redirect})
+
+def reg_view(request):
+
+    if request.method == 'POST':
+        pwd = request.POST.get('pwd')
+        confirm_pwd = request.POST.get('confirmPwd')
+        if pwd != confirm_pwd:
+            pwdDontMatch = True
+            return render(request, 'auth/register.html', {'redirect': '/login/', 'pwdDontMatch': pwdDontMatch})
+        else:
+            pwdDontMatch = False
+            user = get_user_model().objects.create_user(username=request.POST.get('username'),
+                                 password=request.POST.get('pwd'))
+            return HttpResponseRedirect('/login/')
+    return render(request, 'auth/register.html', {'redirect': '/login/', 'pwdDontMatch': False})
