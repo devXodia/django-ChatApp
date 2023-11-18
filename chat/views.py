84ac95ce07fbb46309dadb6ca96c20db9c66c239
Alen Alduk <devalen@alens-mbp.speedport.ip>
2023-11-18 14:05:53 +0100

    added master template

--- chat/views.py
+++ chat/views.py
@@ -1,6 +1,10 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from .models import Chat, Message
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 
+@login_required(login_url='/login/')
 def index(request):
     if request.method == 'POST':
         print('Recieved data ' + request.POST['textmessage'])
@@ -8,3 +12,15 @@ def index(request):
         Message.objects.create(text = request.POST['textmessage'], chat = myChat, author = request.user, receiver = request.user)
     chatMessages = Message.objects.filter(chat__id = 1)
     return render(request, 'chat/index.html', {'messages': chatMessages})
+
+
+def login_view(request):
+    redirect = request.GET.get('next')
+    if request.method == 'POST':
+        user = authenticate(username = request.POST.get('username'), password = request.POST.get('password'))
+        if user:
+            login(request, user)
+            return HttpResponseRedirect(request.POST.get('redirect'))
+        else:
+            return render(request, 'auth/login.html', {'wrongPassword': True, 'redirect': redirect})
+    return render(request, 'auth/login.html', {'redirect': redirect})
