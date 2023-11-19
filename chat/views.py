9e143cae3ebfb1ba72c5ae4c75e0dba6a12fa296
Alen Alduk <a.alduk@icloud.com>
2023-11-19 13:15:47 +0100

    finishing chat app

--- chat/views.py
+++ chat/views.py
@@ -5,13 +5,17 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.contrib.auth import get_user_model
+from django.http import JsonResponse
+from django.core import serializers
 
 @login_required(login_url='/login/')
 def index(request):
     if request.method == 'POST':
         print('Recieved data ' + request.POST['textmessage'])
         myChat = Chat.objects.get(id=1)
-        Message.objects.create(text = request.POST['textmessage'], chat = myChat, author = request.user, receiver = request.user)
+        new_msg = Message.objects.create(text = request.POST['textmessage'], chat = myChat, author = request.user, receiver = request.user)
+        serialized_obj = serializers.serialize('json', [new_msg])
+        return JsonResponse(serialized_obj[1:-1], safe=False)
     chatMessages = Message.objects.filter(chat__id = 1)
     return render(request, 'chat/index.html', {'messages': chatMessages})
 
@@ -38,6 +42,6 @@ def reg_view(request):
         else:
             pwdDontMatch = False
             user = get_user_model().objects.create_user(username=request.POST.get('username'),
-                                 password=request.POST.get('pwd'))
+                                 password=request.POST.get('pwd'), first_name=request.POST.get('username'))
             return HttpResponseRedirect('/login/')
     return render(request, 'auth/register.html', {'redirect': '/login/', 'pwdDontMatch': False})
